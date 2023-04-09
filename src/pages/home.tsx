948f854a28443820040cfa5a7659c59b8c67661d
Ethan TAYLAN <ethantaylan@macbook-pro-de-ethan.home>
2023-04-09 06:37:45 +0200

    used fastpush

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { Test } from "../components/_setupContext";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-type Props = {};
+export const Home = () => {
+  fetch("https://api.storerestapi.com/products/running-sneaker")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
-export const Home = (props: Props) => {
-  return (
-    <div>
-      <Test />
-    </div>
-  );
+  return <h1>hello</h1>;
 };
